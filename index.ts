cf92b5f6d0074af3e350f645414289bca4983e42
Denis Carriere <carriere.denis@gmail.com>
2016-10-13 17:34:27 -0400

    Drop Exact function #5 Replace this with a confidence score

--- index.ts
+++ index.ts
@@ -21,44 +21,6 @@ const defaultQuery = gql`
    }
  }
 `
-
-export async function geocodeExact(address: string, query = defaultQuery): Promise<GoogleResults> {
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${ encodeURI(address) }&sensor=false`
-  let response: any
-  let json: GoogleResults
-
-  let promise: Promise<GoogleResults> = new Promise(async (resolve, reject) => {
-    try {
-      response = await fetch(url)
-    } catch (error) {
-      reject(Error(`Couldn't connect to Geocoding API.`))
-    }
-
-    try {
-      json = response.json()
-    } catch (error) {
-      return reject(Error(`Failed to get json.`))
-    }
-
-    let preciseResults = json.results.filter((result) => {
-      if (result.geometry.location_type !== 'APPROXIMATE') {
-        return result
-      }
-    })
-
-    if (preciseResults.length > 0) {
-      try {
-        return resolve(graphql(defaultResolver, query, {results: preciseResults}))
-      } catch (error) {
-        return reject(Error(error))
-      }
-    } else {
-      return reject(Error(`All results were approximate`))
-    }
-  })
-  return promise
-}
-
 async function parseJSON(response: Response): Promise<GoogleResults> {
   if (response.status !== 200) { throw new Error(response.statusText) }
   const json: GoogleResults = await response.json()
